4a45b3d2a8dae251301ed550abbf24eebe7dbe0b
valdacus <waldemar.sala@dev12.pl>
2025-09-18 10:03:46 +0200

    init

---
+++ src/links/links.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class LinksService {
+  constructor() {} // Możesz tu wstrzyknąć PrismaService
+
+  create(createLinkDto) {
+    // TODO:
+  }
+
+  findAll() {
+    // TODO:
+  }
+
+  findOneAndIncrement(slug: string) {
+    // TODO:
+  }
+}
